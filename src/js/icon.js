ba3ad01145413210bab316d33a39e8503e42d093
vladimir <vladimirpisacic@gmail.com>
2019-02-28 01:03:17 +0000

    first commit

---
+++ src/js/icon.js
@@ -0,0 +1,7 @@
+import fontawesome from "@fontawesome/fontawesome";
+import faClock from "@forawesome/fontawesome-free-regular/faCheck";
+fontawesome.config = {
+	autoReplaceSvg: "nest"
+};
+
+fontawesome.library.add(faClock);
